288d3439740c047f6b06affbde4fab7b456c7f72
Stephane Guichard <stephane.gcrd@gmail.com>
2019-12-10 17:20:54 +0100

    Updates

---
+++ precache-manifest.f53003ecbb9809fc085264816e48350c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da0b9986cbba14e79e6d45d34ac3eb0",
+    "url": "/spotify-network-front/index.html"
+  },
+  {
+    "revision": "b060b9b60dbd175e6080",
+    "url": "/spotify-network-front/static/css/main.6c167a1a.chunk.css"
+  },
+  {
+    "revision": "b0bfd8cba8b5fbc361b7",
+    "url": "/spotify-network-front/static/js/2.ef923086.chunk.js"
+  },
+  {
+    "revision": "b060b9b60dbd175e6080",
+    "url": "/spotify-network-front/static/js/main.e3772c3e.chunk.js"
+  },
+  {
+    "revision": "f9eb46488e7cea1935ff",
+    "url": "/spotify-network-front/static/js/runtime-main.83eda833.js"
+  },
+  {
+    "revision": "f5f82c906c90390912b1b50bb67e2b11",
+    "url": "/spotify-network-front/static/media/draw.f5f82c90.svg"
+  }
+]);
